72d72edf6975863e7ab9afa4c56af9cff2a257e0
CaiqueMeza <caiquemeza.78@outlook.com>
2024-05-27 02:09:26 -0300

    atualização

--- AveragePrice/preco_medio.py
+++ AveragePrice/preco_medio.py
@@ -10,12 +10,16 @@ except ValueError:
 lista_valores = []
 lista_quantidade = []
 mult_matriz = []
+PM_iteracao = []
+quant_iterada = []
 
-i=1
+i = 1
 while i <= repeticao:
     try:
-        lista_valores.append(float(input(f"Coloque o preço {i}: ")))
-        lista_quantidade.append(int(input(f"Coloque a quantidade {i}: ")))
+        preco = float(input(f"Coloque o preço {i}: "))
+        quantidade = int(input(f"Coloque a quantidade {i}: "))
+        lista_valores.append(preco)
+        lista_quantidade.append(quantidade)
         i += 1
     except ValueError:
         print("Entrada inválida. Por favor, insira um valor numérico")
@@ -24,27 +28,37 @@ if repeticao > 0:
     mult_matriz = [x * y for x, y in zip(lista_valores, lista_quantidade)]
     soma_quantidade = sum(lista_quantidade)
     preco_medio = sum(mult_matriz) / soma_quantidade
-    print(f"\nO Preço médio é: R$ {preco_medio: .2f} e a quantidade de ações são:{soma_quantidade: .0f}")
+    
+    # Calcular PM_iteracao e quant_iterada
+    soma_parcial = 0
+    for index in range(repeticao):
+        soma_parcial += lista_quantidade[index]
+        quant_iterada.append(soma_parcial)
+        PM_iteracao.append(sum(mult_matriz[:index+1]) / quant_iterada[index])
+
+    print(f"\nO Preço médio é: R$ {preco_medio:.2f} e a quantidade de ações são: {soma_quantidade:.0f}")
+    
     if str(input("Gostaria de exportar o resultado em .txt? (y/n): ")).lower() == 'y':
         def obter_data_hora_atual():
             return datetime.now().strftime("%d/%m/%Y, %Hh%Mm%Ss")
+        
         data_hora = obter_data_hora_atual()
-        arquivo_txt = open('Py_Lessions\AveragePrice\dados_preco_medio.txt', 'w')
-        caminho_absoluto = os.path.abspath('Py_Lessions\AveragePrice\dados_preco_medio.txt')
-        arquivo_txt.write(f"Este arquivo está salvo em {caminho_absoluto}\nData e hora de criação:{data_hora}\n -------------------------")
-        arquivo_txt.write("\n"*3)
-        arquivo_txt.write(f"Preço médio: R$ {preco_medio}\nQuantidade: {soma_quantidade}")
-        arquivo_txt.write(f"\n\nMemória de Cálculo:\nRepetição: {repeticao}\nLista de Preços: {lista_valores}\n")
-        arquivo_txt.write(f"Lista de Quantidades: {lista_quantidade}\nMatriz de Preços: {mult_matriz}")
-        arquivo_txt.write(f"\nObrigado por usar, desenvolvido por Caique.\n-------------------------")
-        print(f"\nDados importados com sucesso em {caminho_absoluto}")
-        print("\nObrigado por usar a calculadora de preço médio!")
-        arquivo_txt.close()
-    elif str(input("Gostaria de ver a memória de cálculo? (y/n): ")).lower == 'y':
+        caminho_relativo = 'Py_Lessions/AveragePrice/dados_preco_medio.txt'
+        caminho_absoluto = os.path.abspath(caminho_relativo)
+        
+        with open(caminho_relativo, 'w') as arquivo_txt:
+            arquivo_txt.write(f"Este arquivo está salvo em {caminho_absoluto}\nData e hora de criação: {data_hora}\n-------------------------\n\n")
+            arquivo_txt.write(f"Preço médio: R$ {preco_medio:.2f}\nQuantidade: {soma_quantidade}\n\n")
+            arquivo_txt.write(f"Memória de Cálculo:\nRepetição: {repeticao}\nLista de Preços: {lista_valores}\nLista de Quantidades: {lista_quantidade}\n")
+            arquivo_txt.write(f"Matriz de Preços: {mult_matriz}\nQuantidade Iterada: {quant_iterada}\nPM Iterado: {PM_iteracao}\n")
+            arquivo_txt.write("\nObrigado por usar, desenvolvido por Caique.\n-------------------------")
+        
+        print(f"\nDados exportados com sucesso em {caminho_absoluto}")
+    elif str(input("Gostaria de ver a memória de cálculo? (y/n): ")).lower() == 'y':
         print("\nMemória de cálculo:")
         print(f"Repetição: {repeticao}\nLista de Preços: {lista_valores}")
         print(f"Lista de Quantidades: {lista_quantidade}\nMatriz de Preços: {mult_matriz}")
-        print("\nObrigado por usar a calculadora de preço médio!")
+        print(f"Quantidade Iterada: {quant_iterada}\nPM Iterado: {PM_iteracao}")
     else:
         print("\nObrigado por usar a calculadora de preço médio!")
 else:
